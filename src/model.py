852559a979639b3e01cb97eea733f7336df60eb3
Davide Trani <qux24002@uconn.edu>
2026-09-19 15:31:19 -0400

    Update model.py
    Add MLPRegressor with early stopping

--- src/model.py
+++ src/model.py
@@ -1,14 +1,8 @@
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
 
-housing = fetch_california_housing(as_frame=True)
-
-X = housing.data
-y = housing.target
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+model = MLPRegressor(
+    hidden_layer_sizes=(77,),
+    early_stopping=True
 )
 
-print(X_train.shape)
-print(X_test.shape)
+model.fit(X_train, y_train)
