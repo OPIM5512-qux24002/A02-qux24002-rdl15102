03e8e53353153d5aed316b656a784c479b28f647
Davide Trani <qux24002@uconn.edu>
2026-09-19 15:43:30 -0400

    Restore previous code before train plot

--- src/model.py
+++ src/model.py
@@ -1,3 +1,27 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+
+housing = fetch_california_housing(as_frame=True)
+
+X = housing.data
+y = housing.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print(X_train.shape)
+print(X_test.shape)
+
+from sklearn.neural_network import MLPRegressor
+
+model = MLPRegressor(
+    hidden_layer_sizes=(77,),
+    early_stopping=True
+)
+
+model.fit(X_train, y_train)
+
 import os
 import matplotlib.pyplot as plt
 
